5cede5934e99220bc3fb9cb7b39aca5db80793dd
Mateus-Kent <benriba2003@gmail.com>
2021-08-08 16:59:06 -0300

    feat: adiciona fonte poppins

--- App.tsx
+++ App.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import {useFonts  } from 'expo-font'
+import { Poppins_700Bold, Poppins_600SemiBold } from '@expo-google-fonts/poppins'
+import AppLoading from 'expo-app-loading';
+
 import { Choose } from './src/screens/Choose';
 
 export default function App(){
+ const [fontsLoaded] = useFonts({
+  Poppins_700Bold,
+  Poppins_600SemiBold
+ });
+
+ if(!fontsLoaded){
+  return<AppLoading/>
+ }
   return(
     <Choose />
   );
