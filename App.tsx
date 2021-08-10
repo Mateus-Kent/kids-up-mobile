8ec69c940985c3145c6e9cdd8e93996b5503a2ea
Mateus-Kent <benriba2003@gmail.com>
2021-08-10 00:50:17 -0300

    feat: adiciona rotas

--- App.tsx
+++ App.tsx
@@ -3,7 +3,7 @@ import {useFonts  } from 'expo-font'
 import { Poppins_700Bold, Poppins_600SemiBold } from '@expo-google-fonts/poppins'
 import AppLoading from 'expo-app-loading';
 
-import { Choose } from './src/screens/Choose';
+import { Routes} from './src/routes';
 
 export default function App(){
  const [fontsLoaded] = useFonts({
@@ -15,6 +15,6 @@ export default function App(){
   return<AppLoading/>
  }
   return(
-    <Choose />
+   <Routes />
   );
 }
