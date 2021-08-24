e098ccb00102f932c7e3dcd342923d61104cb295
Mateus-Kent <benriba2003@gmail.com>
2021-08-24 17:31:48 -0300

    feat: adiciona tela de selecionar foto de perfil

---
+++ src/screens/SelectPhoto/index.tsx
@@ -0,0 +1,50 @@
+import React, { useRef } from "react";
+import { LinearGradient } from "expo-linear-gradient";
+import { View, Text } from "react-native";
+import { Form } from "@unform/mobile";
+
+import { styles } from "./styles";
+
+import { PhotoPicker } from "../../components/PhotoPicker";
+import { Input } from "../../components/Input";
+import { ButtonPattern } from "../../components/ButtonPattern";
+
+import { FormHandles } from "@unform/core";
+
+type SelectProps = {
+  navigation: any;
+};
+
+export function SelectPhoto({ navigation }: SelectProps) {
+  const formRef = useRef<FormHandles>(null);
+
+  function handleSubmit(data: any) {}
+
+  return (
+    <LinearGradient style={styles.container} colors={["#2D9CDB", "#2F80ED"]}>
+      <Text style={styles.title}>KIDS UP</Text>
+
+      <View style={styles.ViewContainer}>
+        <Text style={styles.caption}>Registrar</Text>
+
+        <Form ref={formRef} onSubmit={handleSubmit} style={styles.formPhoto}>
+          <PhotoPicker />
+          <Text style={styles.subtitle}>Sua foto</Text>
+
+          <Input
+            name={"username"}
+            placeholder="nome de usuário"
+            marginBottom={30}
+          />
+        </Form>
+
+        <ButtonPattern
+          marginBottom={20}
+          onPress={() => navigation.navigate("MainScreen")}
+        >
+          Próximo
+        </ButtonPattern>
+      </View>
+    </LinearGradient>
+  );
+}
---
+++ src/screens/SelectPhoto/styles.ts
@@ -0,0 +1,52 @@
+import { StyleSheet } from 'react-native'
+
+import { theme } from '../../global/styles';
+
+export const styles = StyleSheet.create({
+ container: {
+  flex: 0,
+  justifyContent: 'space-between',
+  alignItems: 'center',
+  width: '100%',
+ },
+ title: {
+  fontSize: 45,
+  color: '#fff',
+  fontFamily: theme.fonts.title700,
+  marginTop: 35,
+  marginBottom: 20
+ },
+ ViewContainer: {
+  borderTopLeftRadius: 20,
+  borderTopRightRadius: 20,
+  width: '100%',
+  backgroundColor: "#fff",
+  paddingBottom: 77,
+  paddingTop: 37,
+  paddingHorizontal: 45,
+  alignItems: 'center',
+  justifyContent: 'space-between',
+ },
+ caption: {
+  fontSize: 32,
+  color: '#000',
+  fontFamily: theme.fonts.title700,
+  marginBottom: 30,
+ },
+ formPhoto: {
+  alignItems: 'center',
+  justifyContent: 'center',
+ },
+ subtitle:{
+  fontFamily: theme.fonts.title600,
+  marginBottom: 30,
+  marginTop: 11,
+  fontSize: 16
+ },
+ label: {
+  fontSize: 14,
+  fontFamily: theme.fonts.title600,
+  color: '#000',
+  marginLeft: 4
+ },
+});
