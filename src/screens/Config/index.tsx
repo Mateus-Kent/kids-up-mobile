0baca891d4ec57c690628d20cf4c26679db24914
Mateus-Kent <benriba2003@gmail.com>
2021-08-27 20:10:36 -0300

    feat: adiciona navegação para tela de EditProfile

--- src/screens/Config/index.tsx
+++ src/screens/Config/index.tsx
@@ -7,7 +7,11 @@ import { styles } from "./styles";
 import { ButtonPattern } from "../../components/ButtonPattern";
 import { WhiteButton } from "../../components/WhiteButton";
 
-export function Config() {
+type ConfigProps = {
+  navigation: any
+  }
+
+export function Config({ navigation }: ConfigProps) {
   return (
     <LinearGradient style={styles.container} colors={["#2D9CDB", "#2F80ED"]}>
       <View style={styles.viewBar}>
@@ -15,7 +19,7 @@ export function Config() {
       </View>
 
       <View style={styles.ViewContainer}>
-        <ButtonPattern marginBottom={18}>Alterar perfil</ButtonPattern>
+        <ButtonPattern marginBottom={18} onPress={() => navigation.navigate('EditProfile')} >Alterar perfil</ButtonPattern>
 
         <ButtonPattern marginBottom={100}>Alterar senha</ButtonPattern>
 
