47f4de169da1b456e999f363359cb5d0e46778f3
Mateus-Kent <benriba2003@gmail.com>
2021-08-30 15:17:08 -0300

    feat: adiciona status bar transparente

--- App.tsx
+++ App.tsx
@@ -3,9 +3,10 @@ import { useFonts } from "expo-font";
 import {
   Poppins_700Bold,
   Poppins_600SemiBold,
-  Poppins_500Medium
+  Poppins_500Medium,
 } from "@expo-google-fonts/poppins";
 import AppLoading from "expo-app-loading";
+import { StatusBar } from "react-native";
 
 import { Routes } from "./src/routes";
 
@@ -13,10 +14,15 @@ export default function App() {
   const [fontsLoaded] = useFonts({
     Poppins_700Bold,
     Poppins_600SemiBold,
-    Poppins_500Medium
+    Poppins_500Medium,
   });
 
   if (!fontsLoaded) {
+    <StatusBar
+      barStyle="light-content"
+      backgroundColor="transparent"
+      translucent
+    />;
     return <AppLoading />;
   }
   return <Routes />;
