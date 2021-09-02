6bb48ac25fbc4849743520b554efb6543e197766
Mateus-Kent <benriba2003@gmail.com>
2021-09-02 15:47:47 -0300

    feat: adiciona input de mandar mensagem

---
+++ src/components/InputMessage/styles.ts
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const style =  StyleSheet.create ( {
+ styleInput: {
+  height: 60,
+  width: 258,
+  borderRadius: 15,
+  borderWidth: 1,
+  paddingHorizontal: 20,
+  backgroundColor: '#EEEDED'
+ }
+} )
