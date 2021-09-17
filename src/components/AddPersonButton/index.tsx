3328ffb1081c9557ec9b90e4062150a8a503e8e8
Mateus-Kent <benriba2003@gmail.com>
2021-09-16 22:57:01 -0300

    feat: adiciona navegação

--- src/components/AddPersonButton/index.tsx
+++ src/components/AddPersonButton/index.tsx
@@ -4,9 +4,16 @@ import { Ionicons } from "@expo/vector-icons";
 
 import { styles } from "./styles";
 
-export function AddPersonButton() {
+type AddProps = {
+  navigation: any;
+};
+
+export function AddPersonButton({ navigation }: AddProps) {
   return (
-    <RectButton style={styles.add}>
+    <RectButton
+      style={styles.add}
+      onPress={() => navigation.navigate("AddContact")}
+    >
       <Ionicons name="md-person-add-outline" size={24} color="#fff" />
     </RectButton>
   );
